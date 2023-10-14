563d5b7b0c3b0afbc53e74c337da6731614a4254
Hai <102190011@sv1.dut.udn.vn>
2023-10-15 01:01:50 +0700

    Delete-Order

--- amazon/backend/routes/orderRoutes.js
+++ amazon/backend/routes/orderRoutes.js
@@ -92,6 +92,21 @@ orderRouter.get(
   })
 );
 
+orderRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.deleteOne();
+      res.send({ message: 'Order Deleted' });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
 orderRouter.put(
   '/:id/deliver',
   isAuth,
